26b1436239ae08bd86c9fa836613625fab7291ab
Maria Serebrennikova <maria.silver2015@yandex.ru>
2022-08-16 18:21:54 +0300

    fix: header bag

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,20 @@
 import React from "react";
 import logo from "../../images/logo.svg";
-// import ExitHeader from "../ExitHeader/ExitHeader";
+import ExitHeader from "../ExitHeader/ExitHeader";
 import "../Header/header.css";
 import Navigation from "../Navigation/Navigation";
 import { Link } from "react-router-dom";
 
 function Header({ isLoggedIn }) {
   return (
-    <header className='header header_theme_light'>
-    {/* <header className={`header ${isLoggedIn ? "header_theme_light" : ""}`}> */}
+    // <header className='header header_theme_light'>
+    <header className={`header ${isLoggedIn ? "header_theme_light" : ""}`}>
       <Link to="/">
         <img className="header__logo" src={logo} alt="Логотип" />
       </Link>
-      <Navigation/>
-      {/* <ExitHeader /> */}
-      {/* {isLoggedIn ? <Navigation /> : <ExitHeader />} */}
+      {/* <Navigation/>
+      <ExitHeader /> */}
+      {isLoggedIn ? <Navigation /> : <ExitHeader />}
     </header>
   );
 }
